782a35e08dad77beb3f224034561068a81432e59
Bigsby76 <118056064+Bigsby76@users.noreply.github.com>
2023-01-28 15:09:45 +0100

    Pyhon Laboratory - code cleared from unnecessary comments from previous tests

--- TP_6_Python/TP5.py
+++ TP_6_Python/TP5.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 from __future__ import print_function
-import sys
 import scipy.io.wavfile as wavfile
-import scipy.fftpack
 import scipy
+import scipy.signal
+import scipy.fftpack
+
 import numpy as np
-import matplotlib.pyplot as plt
-
-#total = len(sys.argv)
-#cmdargs = str(sys.argv)
-#print("total nber of args passed to the script: %d" % total)
-#print ("script name: %s" % str(sys.argv[0]))
-#print ("first argument: %s" % str(sys.argv[1]))
+from matplotlib import pyplot as plt
 
 
-""" fs_rate, signal = wavfile.read("LAB5_500HzFHR.wav")
+fs_rate, signal = wavfile.read("LAB5_500HzFHR.wav")
 print ("Frequency sampling", fs_rate)
 l_audio = len(signal.shape)
 print ("Channels", l_audio)
@@ -29,32 +23,41 @@ secs = N / float(fs_rate)
 print ("secs", secs)
 Ts = 1.0/fs_rate # sampling interval in time
 print ("Timestep between samples Ts", Ts)
-t = scipy.arange(0, secs, Ts) # time vector as scipy arange field / numpy.ndarray
-FFT = abs(scipy.fft(signal))
-FFT_side = FFT[range(N/2)] # one side FFT range
+t = np.arange(0, secs, Ts) # time vector as scipy arange field / numpy.ndarray
+FFT_full = abs(scipy.fft.fft(signal))
+FFT_side = FFT_full[range(N//2)] # one side FFT range
 freqs = scipy.fftpack.fftfreq(signal.size, t[1]-t[0])
 fft_freqs = np.array(freqs)
-freqs_side = freqs[range(N/2)] # one side frequency range
+freqs_side = freqs[range(N//2)] # one side frequency range
 fft_freqs_side = np.array(freqs_side)
-plt.subplot(311)
+
+#Filtering Low pqss filter
+b, a = scipy.signal.butter(10, 0.02, 'low')
+output_signal = scipy.signal.filtfilt(b, a, signal)
+#fft of the filtered signal
+FFT_full_filter = abs(scipy.fft.fft(output_signal))
+FFT_side_filter = FFT_full_filter[range(N//2)] # one side FFT range
+
+
+
+plt.subplot(511)
 p1 = plt.plot(t, signal, "g") # plotting the signal
 plt.xlabel('Time')
 plt.ylabel('Amplitude')
-plt.subplot(312)
-p2 = plt.plot(freqs, FFT, "r") # plotting the complete fft spectrum
+plt.subplot(512)
+p2 = plt.plot(freqs, FFT_full, "r") # plotting the complete fft spectrum
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Count dbl-sided')
-plt.subplot(313)
+plt.subplot(513)
 p3 = plt.plot(freqs_side, abs(FFT_side), "b") # plotting the positive fft spectrum
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Count single-sided')
-plt.show() """
-
-
-fs, data = wavfile.read('LAB5_500HzFHR.wav') # load the data
-a = data.T[0] # this is a two channel soundtrack, I get the first track
-b=[(ele/2**8.)*2-1 for ele in a] # this is 8-bit track, b is now normalized on [-1,1)
-c = scipy.fft(b) # calculate fourier transform (complex numbers list)
-d = len(c)/2  # you only need half of the fft list (real signal symmetry)
-plt.plot(abs(c[:(d-1)]),'r') 
+plt.subplot(514)
+p4 = plt.plot(t, output_signal, "g") # plotting the filtered signal
+plt.xlabel('Time')
+plt.ylabel('Amplitude filtered')
+plt.subplot(515)
+p5 = plt.plot(freqs_side, FFT_side_filter, "b") # plotting the filtered signal
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Count single-sided filtered signal')
 plt.show()
